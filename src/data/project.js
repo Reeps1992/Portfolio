ea6afcb0225393f14cc197bb60203b083b67e6d6
Kévin Conversin <conversin.k@gmail.com>
2021-01-22 15:19:35 +0100

    fixing errors

--- src/data/project.js
+++ src/data/project.js
@@ -4,7 +4,7 @@ export default [
         name : 'faquarium',
         path : 'http://kevin-conversin-dev.ovh/index.php?p=home',
         date : '03/20',
-        desc : 'Faux site pour aquarium pour un projet de formation, réalisé avec HTML / CSS / JS / Jquery / PHP',
+        desc : 'Faux site pour aquarium pour un projet de formation, made with HTML / CSS / JS / Jquery / PHP',
         repo : 'https://github.com/Reeps1992/Faquarium',
         img : 'https://media.istockphoto.com/photos/serious-boy-looking-in-aquarium-with-tropical-fish-picture-id1055171006?k=6&m=1055171006&s=612x612&w=0&h=dEt0Bn_mk08D4ur8pl4ZPWH5jLoLYYEqCaTIKWMj91Q=',
     },
@@ -13,7 +13,7 @@ export default [
         name : 'apptimizer',
         path : 'http://apptimizer.kevin-conversin-dev.ovh/construct/pages/login.php',
         date : '11/19',
-        desc : 'ERP pour un projet de formation orienté PHP. (Si vous souhaitez y jeter un coup d\'oeil, envoyez moi un MP via LinkedIn)',
+        desc : 'ERP pour un projet de formation afin de m\'améliorer en PHP. (Si vous souhaitez y jeter un coup d\'oeil, envoyez moi un MP via LinkedIn)',
         repo : 'https://github.com/Reeps1992/Apptimizer_01',
         img : 'https://media.istockphoto.com/vectors/enterprise-resource-planning-illustration-concept-vector-id1283052761?k=6&m=1283052761&s=612x612&w=0&h=CfilrsfVQ4k3h1_PJ457sNkyPWDA4QCeGX7vsC3TTtk=',
     },
@@ -25,6 +25,15 @@ export default [
         desc : 'This Portfolio made with React JS',
         repo : 'https://github.com/Reeps1992',
         img : 'https://media.istockphoto.com/photos/minimal-work-space-creative-flat-lay-photo-of-workspace-desk-top-view-picture-id928805632?k=6&m=928805632&s=612x612&w=0&h=cGxMuhinwtaQZeRBs9o5w98fJYoU-vhHmO80YhWlvwE=',
+    },
+    { 
+        id : 4,
+        name : 'O\'Cooking',
+        path : 'http://ec2-100-25-30-18.compute-1.amazonaws.com/',
+        date : '12/20',
+        desc : 'This project was made with my team during my studies on React.JS & Redux.',
+        repo : 'https://github.com/Reeps1992',
+        img : 'http://ec2-100-25-30-18.compute-1.amazonaws.com/api/img/recipes/salade-cesar-titi007-3.jpeg',
     },
     
 ]
